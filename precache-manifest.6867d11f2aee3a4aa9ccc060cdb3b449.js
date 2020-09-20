a222a34418212999820993f2863d33fd4a30f42a
Yuriy Nagaev <yuriy@nagaev.me>
2020-09-20 14:14:57 +0700

    release 1

---
+++ precache-manifest.6867d11f2aee3a4aa9ccc060cdb3b449.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ba9ff2904d4475c302dbebcaa90a5b",
+    "url": "https://thaidriver.github.io/license-test/index.html"
+  },
+  {
+    "revision": "97397e681bb11d63befd",
+    "url": "https://thaidriver.github.io/license-test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bbda8c147375055ac3cd",
+    "url": "https://thaidriver.github.io/license-test/static/js/2.ceadc7b3.chunk.js"
+  },
+  {
+    "revision": "96daf2f82d179022f535a23c00ffb1f2",
+    "url": "https://thaidriver.github.io/license-test/static/js/2.ceadc7b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed053496797cb612b8db",
+    "url": "https://thaidriver.github.io/license-test/static/js/3.653bb539.chunk.js"
+  },
+  {
+    "revision": "97397e681bb11d63befd",
+    "url": "https://thaidriver.github.io/license-test/static/js/main.5e62749e.chunk.js"
+  },
+  {
+    "revision": "594cdfa8fe090ce16446",
+    "url": "https://thaidriver.github.io/license-test/static/js/runtime-main.afcbf0c3.js"
+  }
+]);
